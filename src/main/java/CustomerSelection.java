75c280c04e298731545af1539aee44278d7d1fdc
Yasin Yılmaz <yasinyilmazzz@gmail.com>
2022-11-16 16:07:15 +0300

    project Grosary start

--- src/main/java/CustomerSelection.java
+++ src/main/java/CustomerSelection.java
@@ -1,28 +1,59 @@
+import javax.sql.rowset.Predicate;
 import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class CustomerSelection {
     Double shoppingCartTotal;
-
-
-
-
-
-
-
+    HashMap<String,Integer> shoppingCart=new HashMap<String, Integer>();
     Scanner input = new Scanner(System.in);
+
+    //to encapsulate variables
+    public Double getShoppingCartTotal() {return shoppingCartTotal;    }
+    public void setShoppingCartTotal(Double shoppingCartTotal) {this.shoppingCartTotal = shoppingCartTotal;    }
+    public HashMap<String, Integer> getShoppingCart() {return shoppingCart;    }
+    public void setShoppingCart(HashMap<String, Integer> shoppingCart) {this.shoppingCart = shoppingCart;    }
+    public Scanner getInput() {return input;    }
+    public void setInput(Scanner input) {this.input = input;    }
+
+
+    //Money Formatter
     DecimalFormat moneyFormat = new DecimalFormat("'$'###,##0.00");
 
-    private Double addingProduct (double prize){
-        Double amount = 0.0;
+
+
+    private Double addingProductPrize (double prize, double amount) {
         shoppingCartTotal+=prize*amount;
         return shoppingCartTotal;
     }
 
-    private Double substractProoduct (double prize, double amount) {
+    private Double substractProoductPrize (double prize, double amount) {
         shoppingCartTotal -= prize * amount;
         return shoppingCartTotal;
     }
+
+    private HashMap addingProductToCart(String product, Integer amount){
+        if (shoppingCart.get(product)!=0){
+            shoppingCart.replace(product,shoppingCart.get(product)+amount);
+        }
+        shoppingCart.putIfAbsent(product, amount);
+
+        return shoppingCart;
+    }
+
+    private HashMap removeProductToCart (String product, Integer amount){
+        if (amount==shoppingCart.get(product)){
+            shoppingCart.remove(product);
+        }
+        shoppingCart.replace(product,shoppingCart.get(product)-amount);
+
+        return shoppingCart;
+    }
+
+
+
+
+
+
 
 }
